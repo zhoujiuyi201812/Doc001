7d267ca7e422df993a54ad84f59fa2db9e781d00
zhoujiuyi201812 <83483147+zhoujiuyi201812@users.noreply.github.com>
2026-05-16 13:29:01 +0800

    Update CodeTest.java

--- CodeTest.java
+++ CodeTest.java
@@ -1,80 +1,30 @@
-  import java.io.*;
 import java.util.*;
 
 class Solution {
-    static class FastScanner {
-        private final InputStream in;
-        private final byte[] buffer = new byte[1 << 16];
-        private int ptr = 0, len = 0;
-
-        FastScanner(InputStream is) {
-            in = is;
-        }
-
-        private int read() throws IOException {
-            if (ptr >= len) {
-                len = in.read(buffer);
-                ptr = 0;
-                if (len <= 0) return -1;
-            }
-            return buffer[ptr++];
-        }
-
-        int nextInt() throws IOException {
-            int c;
-            do {
-                c = read();
-            } while (c <= ' ' && c != -1);
-
-            int sign = 1;
-            if (c == '-') {
-                sign = -1;
-                c = read();
-            }
-
-            int val = 0;
-            while (c > ' ') {
-                val = val * 10 + c - '0';
-                c = read();
-            }
-            return val * sign;
-        }
-    }
-
     public static void main(String args[]) throws Exception {
-        FastScanner fs = new FastScanner(System.in);
-        int n = fs.nextInt();
-        int k = fs.nextInt();
+        Scanner sc = new Scanner(System.in);
+        int radioCap = sc.nextInt();
+        int numLiq = sc.nextInt();
+        int mass = sc.nextInt();
 
-        if (n <= 0 || k <= 0) {
-            return;
-        }
+        int[] vol = new int[numLiq];
+        int[] wt = new int[numLiq];
+        int[] energy = new int[numLiq];
 
-        int[] arr = new int[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = fs.nextInt();
-        }
+        for (int i = 0; i < numLiq; i++) vol[i] = sc.nextInt();
+        for (int i = 0; i < numLiq; i++) wt[i] = sc.nextInt();
+        for (int i = 0; i < numLiq; i++) energy[i] = sc.nextInt();
 
-        ArrayDeque<Integer> q = new ArrayDeque<Integer>();
-        StringBuilder sb = new StringBuilder();
+        long[][] dp = new long[radioCap + 1][mass + 1];
 
-        for (int i = 0; i < n; i++) {
-            if (!q.isEmpty() && q.peekFirst() <= i - k) {
-                q.pollFirst();
-            }
-
-            if (arr[i] < 0) {
-                q.offerLast(i);
-            }
-
-            if (i >= k - 1) {
-                if (sb.length() > 0) {
-                    sb.append(' ');
+        for (int i = 0; i < numLiq; i++) {
+            for (int v = radioCap; v >= vol[i]; v--) {
+                for (int m = mass; m >= wt[i]; m--) {
+                    dp[v][m] = Math.max(dp[v][m], dp[v - vol[i]][m - wt[i]] + energy[i]);
                 }
-                sb.append(q.isEmpty() ? 0 : arr[q.peekFirst()]);
             }
         }
 
-        System.out.print(sb.toString());
+        System.out.println(dp[radioCap][mass]);
     }
 }
